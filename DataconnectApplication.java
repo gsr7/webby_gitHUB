61e5a885e065588d312f9066efa83c0bb3ec816c
gsr7 <43493730+gsr7@users.noreply.github.com>
2019-10-07 20:17:46 +1100

    Added comments in DataApplication.java file
    Commit from Branch - 2 at 20.17

--- DataconnectApplication.java
+++ DataconnectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DataconnectApplication {
-
+// adding Comments just to know Program to New Developers
 	public static void main(String[] args) {
 		SpringApplication.run(DataconnectApplication.class, args);
 	}
