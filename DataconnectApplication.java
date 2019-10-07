54cacd1659878951b46e0ffb6721367cfd02acd8
gsr7 <43493730+gsr7@users.noreply.github.com>
2019-10-07 20:15:38 +1100

    Added DataconnectApplication.java file at 20.14
    Add extended description from Branch - 2

---
+++ DataconnectApplication.java
@@ -0,0 +1,13 @@
+package com.app.dbapp.dataconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataconnectApplication.class, args);
+	}
+
+}
